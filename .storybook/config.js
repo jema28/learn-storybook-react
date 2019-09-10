9bf2fdae1d5ad6a91803ab72dae1cf12730db17e
Jemila <jemila.abulhawa@gmail.com>
2019-09-10 11:16:14 +0300

    set up automated test with snapshots

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,9 +1,10 @@
 import { configure } from '@storybook/react'
+import requireContext from 'require-context.macro'
 import '../src/index.css'
 
 // By default Storybook looks for stories in a /stories directory
 // Instead we want it to look for stories as `.stories.js`
-const req = require.context('../src', true, /\.stories.js$/)
+const req = requireContext('../src/components', true, /\.stories\.js$/)
 
 function loadStories() {
   req.keys().forEach(filename => req(filename))
