b12310ada700344f6389a3027f350e07b4ad14ad
Jemila <jemila.abulhawa@gmail.com>
2019-09-10 15:34:20 +0300

    wire in data using redux

--- src/components/TaskList/TaskList.test.js
+++ src/components/TaskList/TaskList.test.js
@@ -1,12 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import TaskList from './index'
+import { PureTaskList } from './index'
 import { withPinnedTasks } from './TaskList.stories'
 
 it('renders pinned tasks at the start of the list', () => {
   const div = document.createElement('div')
   const events = { onPinTask: jest.fn(), onArchiveTask: jest.fn() }
-  ReactDOM.render(<TaskList tasks={withPinnedTasks} {...events} />, div)
+  ReactDOM.render(<PureTaskList tasks={withPinnedTasks} {...events} />, div)
 
   // We expect the task titled "Task 6 (pinned)" to be rendered first, not at the end
   const lastTaskInput = div.querySelector(
